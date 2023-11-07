febe4df6ca6a4e17f5ff9436ae61d5a3108d2807
Mateo A <67398321+gobrowse@users.noreply.github.com>
2023-11-07 06:13:33 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Gobot'
+copyright = '2023, Mateo A'
+author = 'Mateo A'
 
 release = '0.1'
 version = '0.1.0'
